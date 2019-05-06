b029990b10b8247c957451618d76d70ac78cdeec
zhoufeiyu <119775549@qq.com>
2019-05-06 15:47:13 +0800

    install mysql

--- studyBackend/studyBackend/settings.py
+++ studyBackend/studyBackend/settings.py
@@ -127,9 +127,17 @@ WSGI_APPLICATION = 'studyBackend.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PLAN',
+        'USER': 'zhoufeiyu',
+        'PASSWORD': 'zfy1996514',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
